67584f9f2b93f1e84cd7780b6e2e5173a7bbcd61
spmulders <info@sebastiaanmulders.nl>
2019-05-21 13:26:01 +0200

    Update compileDISCON_linux.bash

--- Scripts/compileDISCON_linux.bash
+++ Scripts/compileDISCON_linux.bash
@@ -8,9 +8,11 @@ sed -i '14s/.*/BITS = 64/' makefile
 
 # Build code
 rm -f ../DISCON/DISCON_glin64.so
+rm -f ../DISCON_x64_DRC.so
 make clean 
 make all
-echo 'The output file is: "../DISCON/DISCON_glin64.so".' 
+mv ../DISCON/DISCON_glin64.so ../DISCON_x64_DRC.so
+echo 'The output file is: "../DISCON_x64_DRC.so".' 
 
 # Return to initial directory
 cd ../Scripts
