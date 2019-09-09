d36eaaa578e7e24a1dc64bda212aa9f3ea12fb81
Paul <paul.fleming@nrel.gov>
2019-09-09 16:35:11 -0600

    first commit

---
+++ examples/example_05.py
@@ -0,0 +1,30 @@
+# Example_05
+# make some test controller calls
+
+
+from WTC_toolbox import turbine as wtc_turbine
+from WTC_toolbox import controller as wtc_controller
+# from WTC_toolbox import sim as wtc_sim
+from WTC_toolbox import control_interface as ci
+import numpy as np
+
+# Some useful constants
+degRad = np.pi/180.
+rpmRadSec = 2.0*(np.pi)/60.0
+
+# Initialiize a controller interface class
+lib_name = 'test_controller/DISCON_glin64.so'
+# lib_name = '/Users/pfleming/Desktop/git_tools/floating/DRC_Fortran/DISCON//DISCON_glin64.so'
+controller = ci.ConInt(lib_name)
+
+GBRatio = 97.
+t = 0
+dt = 0.1
+pitch=0
+rotspeed = 10 * rpmRadSec
+genspeed = GBRatio * rotspeed
+ws = 8.0
+
+for i in range(10):
+    controller.show_control_values()
+    controller.call_controller(t,dt,pitch,genspeed,rotspeed,ws)
