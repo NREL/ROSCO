33642641d2675a7ce71ee474d5278d976efccd6f
Nikhar Abbas <nikhar.abbas@colorado.edu>
2019-09-13 11:59:13 -0600

    Update comments

--- WTC_toolbox/sim.py
+++ WTC_toolbox/sim.py
@@ -37,13 +37,28 @@ class Sim():
         self.gen_eff = 0.95
 
 
-    def sim_ws_series(self,t_array,ws_array,rotor_rpm_init=10,init_pitch=0.0):
-        
-        # Store some for conveniente
+    def sim_ws_series(self,t_array,ws_array,rotor_rpm_init=10,init_pitch=0.0, make_plots=True):
+        '''
+        Simulate simplified turbine model using a complied controller (.dll or similar).
+            - currently a 1DOF rotor model
+
+        Parameters:
+        -----------
+            t_array: float
+                     Array of time steps, (s)
+            ws_array: float
+                      Array of wind speeds, (s)
+            rotor_rpm_init: float, optional
+                            initial rotor speed, (rpm)
+            init_pitch: float, optional
+                        initial blade pitch angle, (deg)
+            make_plots: bool, optional
+                        True: generate plots, False: don't. 
+        '''
+        # Store turbine data for conveniente
         dt = t_array[1] - t_array[0]
         R = self.turbine.RotorRad
         GBRatio = self.turbine.Ng
-
 
         # Declare output arrays
         pitch = np.ones_like(t_array) * init_pitch 
@@ -53,7 +68,8 @@ class Sim():
         gen_torque = np.ones_like(t_array) # * trq_cont(turbine_dict, gen_speed[0])
         gen_power = np.ones_like(t_array) * 0.0
 
-
+        # Test for cc_blade Cq information. 
+        #       - If not, assume available matrices loaded from text file, and interpolate those
         try: 
             self.turbine.cc_rotor.evaluate(ws_array[1], [rot_speed[0]/rpm2RadSec], 
                                                         [pitch[0]], 
@@ -67,8 +83,9 @@ class Sim():
         for i, t in enumerate(t_array):
             if i == 0:
                 continue # Skip the first run
-
             ws = ws_array[i]
+
+            # Load current Cq data
             if use_interpolated:
                 tsr = rot_speed[i-1] * self.turbine.RotorRad / ws
                 cq = self.turbine.Cq.interp_surface([pitch[i-1]],tsr)
@@ -77,33 +94,10 @@ class Sim():
                                                         [rot_speed[i-1]/rpm2RadSec], 
                                                         [pitch[i-1]], 
                                                         coefficients=True)
-            # try:
-            #     P, T, Q, M, Cp, Ct, cq, CM = self.turbine.rotor.evaluate([ws], 
-            #                                             [rot_speed[i-1]/rpm2RadSec], 
-            #                                             [pitch[i-1]], 
-            #                                             coefficients=True)
-            # except:
-            #     print('CC BLADE PROBLEM')
-            #     e = sys.exc_info()[0]
-            #     print(e)
-            #     # carry through the past values and continue
-            #     pitch[i] = pitch[i-1]
-            #     rot_speed[i] = rot_speed[i-1]
-            #     gen_speed[i] = gen_speed[i-1]
-            #     aero_torque[i] = aero_torque[i-1]
-            #     gen_torque[i] = gen_torque[i-1]
-            #     gen_power[i] = gen_power[i-1]
-            #     continue
 
+            # Update the turbine state
+            #       -- 1DOF model: rotor speed and generator speed (scaled by Ng)
             aero_torque[i] = 0.5 * self.turbine.rho * (np.pi * R**2) * cq * R * ws**2
-
-            # # Save these values for plotting
-            # cq_array[i] = cq
-            # cp_array[i] = Cp[0]
-            # ct_array[i] = Ct[0]
-            # tsr_array[i] = tsr
-
-            # Update the rotor speed and generator speed
             rot_speed[i] = rot_speed[i-1] + (dt/self.turbine.J)*(aero_torque[i] * self.gb_eff - self.turbine.Ng * gen_torque[i-1])
             gen_speed[i] = rot_speed[i] * self.turbine.Ng 
 
@@ -123,28 +117,23 @@ class Sim():
         self.t_array = t_array
         self.ws_array = ws_array
 
+        if make_plots:
+            fig, axarr = plt.subplots(4,1,sharex=True,figsize=(6,10))
 
-        fig, axarr = plt.subplots(4,1,sharex=True,figsize=(6,10))
-
-
-
-        ax = axarr[0]
-        ax.plot(self.t_array,self.ws_array,label='Wind Speed')
-        ax.grid()
-        ax.legend()
-
-        ax = axarr[1]
-        ax.plot(self.t_array,self.rot_speed,label='Rot Speed')
-        ax.grid()
-        ax.legend()
-
-        ax = axarr[2]
-        ax.plot(self.t_array,self.gen_torque,label='Gen Torque')
-        ax.grid()
-        ax.legend()
-
-        ax = axarr[3]
-        ax.plot(self.t_array,self.pitch,label='Bld Pitch')
-        ax.grid()
-        ax.legend()
+            ax = axarr[0]
+            ax.plot(self.t_array,self.ws_array,label='Wind Speed')
+            ax.grid()
+            ax.legend()
+            ax = axarr[1]
+            ax.plot(self.t_array,self.rot_speed,label='Rot Speed')
+            ax.grid()
+            ax.legend()
+            ax = axarr[2]
+            ax.plot(self.t_array,self.gen_torque,label='Gen Torque')
+            ax.grid()
+            ax.legend()
+            ax = axarr[3]
+            ax.plot(self.t_array,self.pitch,label='Bld Pitch')
+            ax.grid()
+            ax.legend()
         
