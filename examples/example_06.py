f19536cf76780533f6056abac0758186a70504df
Nikhar Abbas <nikhar.abbas@colorado.edu>
2019-09-12 21:40:54 -0600

    Updates to run on Nikhar's machine

--- examples/example_06.py
+++ examples/example_06.py
@@ -7,16 +7,28 @@ from WTC_toolbox import sim as wtc_sim
 from WTC_toolbox import control_interface as ci
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+
+# ensure proper directory location --- this is annoying and should be fixed
+path = os.getcwd()
+os.chdir('%s/examples'%path)
 
 # Load turbine model
 # Initialiize a turbine class
 turbine = wtc_turbine.Turbine()
 
-# Load quick from python
-turbine.load('saved_turbine.p')
+# Load turbine from OpenFAST and *.txt file
+FAST_InputFile = '5MW_Land.fst'
+FAST_directory = '/Users/nabbas/Documents/TurbineModels/NREL_5MW/5MW_Land'
+txt_filename = 'Cp_Ct_Cq.txt'
+drivetrain_inertia = 40469564.444
+turbine.load_from_fast(FAST_InputFile,FAST_directory,drivetrain_inertia,dev_branch=True,rot_source='txt', txt_filename=txt_filename)
+
+# # Load turbine quick from python
+# turbine.load('saved_turbine.p')
 
 # Load controller 
-lib_name = 'test_controller/DISCON_glin64.so'
+lib_name = 'test_controller/DISCON.dll'
 # lib_name = '/Users/pfleming/Desktop/git_tools/floating/DRC_Fortran/DISCON//DISCON_glin64.so'
 controller_int = ci.ConInt(lib_name)
 
