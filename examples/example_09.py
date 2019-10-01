c0bf7c7ff0cc6eab5587543c737aa7dd7822a263
Nikhar Abbas <nikhar.abbas@colorado.edu>
2019-10-01 17:41:59 -0600

    Plot peak shaving values

--- examples/example_09.py
+++ examples/example_09.py
@@ -1,14 +1,15 @@
 # example_09.py
-# Include peak shaving
+# Plot expected shaved rotor thrust 
 
-%##
+#%%
 import numpy as np
-from scipy import interpolate 
+import matplotlib.pyplot as plt 
+import os
 
 from WTC_toolbox import controller as wtc_controller
 from WTC_toolbox import turbine as wtc_turbine
 from WTC_toolbox import sim as wtc_sim
-import os
+
 
 os.chdir('/Users/nabbas/Documents/WindEnergyToolbox/WTC_toolbox/examples')
 
@@ -19,13 +20,24 @@ file_processing = wtc_controller.FileProcessing()
 
 # Fast input file and Cp surface text file
 FAST_InputFile = '5MW_Land.fst'
-FAST_directory = '/Users/nabbas/Documents/TurbineModels/NREL_5MW/5MW_Land'
+FAST_directory = '../Test_Cases/5MW_Land'
 txt_filename = 'Cp_Ct_Cq.txt'
 
 drivetrain_inertia = 40469564.444
 turbine.load_from_fast(FAST_InputFile,FAST_directory,drivetrain_inertia,dev_branch=True,rot_source='txt', txt_filename=txt_filename)
 
-# Load controller
+#%% Load controller
 controller.tune_controller(turbine)
 
-turbine.Ct.interp_surface
+
+#%%
+fig, axs = plt.subplots(2,1)
+axs[0].plot(controller.ps.v, controller.ps.T)
+axs[0].plot(controller.ps.v, controller.ps.Tshaved)
+
+axs[1].plot(controller.ps.v, controller.pitch_op)
+axs[1].plot(controller.ps.v, controller.ps.pitch_min)
+
+plt.show()
+
+#%%
