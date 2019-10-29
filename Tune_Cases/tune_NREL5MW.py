85503a5e134d29ece2ae34c0c33580f1daa4c2e5
Nikhar Abbas <nikhar.abbas@colorado.edu>
2019-10-29 17:53:10 -0600

    Account for fileprocessing re-org

--- Tune_Cases/tune_NREL5MW.py
+++ Tune_Cases/tune_NREL5MW.py
@@ -26,9 +26,8 @@ turbine_params = inps['turbine_params']
 controller_params = inps['controller_params']
 
 #---------------------------------- DO THE FUN STUFF ------------------------------------#
-# Initialiize turbine, controller, and file processing classes
+# Initialiize turbine
 turbine         = wtc_turbine.Turbine(turbine_params)
-controller      = wtc_controller.Controller(controller_params)
 file_processing = wtc_controller.FileProcessing()
 
 # Load Turbine
@@ -37,16 +36,16 @@ if inps['path_params']['rotor_performance_filename']:
 else:
     turbine.load_from_fast(path_params['FAST_InputFile'],path_params['FAST_directory'],dev_branch=True,rot_source=None)
     # Write rotor performance file
-    turbine.write_rotor_performance(txt_filename='Cp_Ct_Cq.txt')
+    file_processing.write_rotor_performance(turbine,txt_filename='Cp_Ct_Cq.txt')
 
-# Tune controller
+# Initialize controller tuning and tune controller
+controller      = wtc_controller.Controller(controller_params)
 controller.tune_controller(turbine)
 
-
-
+# Initialize 
 # Write parameter input file
 param_file = 'DISCON.IN'   # This must be named DISCON.IN to be seen by the compiled controller binary. 
-file_processing.write_param_file(param_file,turbine,controller,new_file=True)
+file_processing.write_param_file(turbine,controller,param_file=param_file)
 
 # plot rotor performance 
 turbine.Cp.plot_performance(turbine.Cp_table, turbine.pitch_initial_rad, turbine.TSR_initial)
