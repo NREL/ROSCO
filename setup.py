cb879bc6db751ce3c6014d1926a730f8a47188e3
Nikhar Abbas <nikhar.abbas@colorado.edu>
2020-01-09 23:27:48 -0700

    Update for version 1 release

--- setup.py
+++ setup.py
@@ -1,62 +1,154 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# Copyright 2019 NREL
+
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+# this file except in compliance with the License. You may obtain a copy of the
+# License at http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
 
 """The setup script."""
 
-from setuptools import setup, find_packages
+# This setup file was made to mimic the setup.py script for https://github.com/NREL/floris/
+# Accessed on January 9, 2020
 
-with open('README.md') as readme_file:
-    readme = readme_file.read()
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
+import io
+import os
+import sys
+from shutil import rmtree
 
-requirements = [
-    'Click>=6.0',
-    # TODO: put package requirements here
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = 'ROSCO_toolbox'
+DESCRIPTION = 'A toolbox for development of wind turbine controllers.'
+URL = 'https://github.com/NREL/ROSO_toolbox'
+EMAIL = 'nikhar.abbas@nrel.gov'
+AUTHOR = 'NREL National Wind Technology Center'
+REQUIRES_PYTHON = '>=3.3.0'
+VERSION = '1.0.0'
+
+# These packages are required for all of the code to be executed. 
+# - Maybe you can get away with older versions...
+REQUIRED = [
+    'matplotlib>=3',
+    'numpy>=1.16',
+    'pytest>=4',
+    'scipy>=1.1',
+    'wisdem>=2.0'   # This is NREL's distrubtion of WISDEM. CC-Blade and Aeroelastic SE are used as of January 2020
+                    # https://github.com/wisdem/wisdem
 ]
 
-setup_requirements = [
-    # TODO(pfleming): put setup requirements (distutils extensions, etc.) here
-]
+# Read the docs, one day, so we'll throw it in here!
+EXTRAS = {
+    'docs': {
+        'readthedocs-sphinx-ext==0.5.15',
+        'Sphinx==2.0',
+        'sphinxcontrib-napoleon==0.7'
+    }
+}
 
-test_requirements = [
-    # TODO: put package test requirements here
-]
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for that!
 
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
+    with open(os.path.join(here, project_slug, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+        
+        sys.exit()
+
+
+# Where the magic happens:
 setup(
-    name='ROSCO_toolbox',
-    version='0.1.0',
-    description="Tools for generic turbine control.",
-    long_description=readme + '\n\n' + history,
-    author="Nikhar J. Abbas",
-    author_email='nikhar.abbas@nrel.gov',
-    url='https://github.com/NREL/ROSCO_toolbox',
-    packages=find_packages(include=['ROSCO_toolbox']),
-    entry_points={
-        'console_scripts': [
-            'ROSCO_toolbox=ROSCO_toolbox.cli:main'
-        ]
-    },
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    # If your package is a single module, use this instead of 'packages':
+    # py_modules=['mypackage'],
+
+    # entry_points={
+    #     'console_scripts': ['mycli=mymodule:cli'],
+    # },
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
     include_package_data=True,
-    install_requires=requirements,
-    license="Apache Software License 2.0",
-    zip_safe=False,
-    keywords='ROSCO_toolbox',
+    license='Apache-2.0',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
         'License :: OSI Approved :: Apache Software License',
-        'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
     ],
-    test_suite='tests',
-    tests_require=test_requirements,
-    setup_requires=setup_requirements,
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
 )
