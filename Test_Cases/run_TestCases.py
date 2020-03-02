a3c39ebe9f2bae275335cee0c4908a9f80187cb6
Nikhar Abbas <nikhar.abbas@colorado.edu>
2020-03-02 10:26:29 -0700

    Update for new test cases

--- Test_Cases/run_TestCases.py
+++ Test_Cases/run_TestCases.py
@@ -1,43 +1,67 @@
-# ------- Run_TestCases -------
-# Run the simulation cases in the Test_Cases folder
-# Plot some basic results to make sure they worked
-# -----------------------------
+''' 
+----------------- Run_TestCases -----------------
+- Run the simulation cases in the Test_Cases folder
+- Plot some basic results to make sure they worked
 
+Notes:
+    By default, the provided test case models point to a compiled controller at:
+    "../../ROSCO/build/libdiscon.dylib". You will need have this compiled, or 
+    change the path to the controller in the ServoDyn input files.
+-------------------------------------------------
+'''
 # Import and define modules and classes
 import os
 import matplotlib.pyplot as plt
 from ROSCO_toolbox import utilities as ROSCO_Utilities
 fast_io = ROSCO_Utilities.FAST_IO()
 
-# Define call for OpenFAST
-fastcall = 'openfast_dev'
+# Define call for OpenFAST and turbsim
+openfast_call    = 'openfast_dev'
+turbsim_call = 'turbsim_dev'
 
 # Define folder names in Test_Cases to run
-testcases = [
-                # '5MW_Step',      # Step wind (General Functionality)
-                '5MW_Turb_NR',   # Near rated class A turbulence (Setpoint smoothing)
-                # '5MW_Turb_NR_ps',   # Near rated class A turbulence (Setpoint smoothing, peak shaving)
-                # '5MW_Ramp'      # Ramp from 5-30 m/s in 200 seconds (Shutdown)
-            ]
+test_cases = ['5MW_Land_DLL_WTurb',
+                '5MW_OC3SPAR_DLL_WTurb_WavesIrr']
+
+# Names of wind turbulent wind binaries
+wind_binaries = ['Wind/90m_12mps_twr.bts']
 
 # Define data to plot 
-cases = {}
-cases['Baseline'] = ['Wind1VelX', 'BldPitch1', 'GenTq', 'RotSpeed']
+plot_categories = {}
+plot_categories['LandBased'] = ['Wind1VelX', 'BldPitch1', 'GenTq', 'RotSpeed']
+plot_categories['Floating'] = ['Wind1VelX', 'BldPitch1', 'GenTq', 'RotSpeed', 'PtfmPitch']
+
+
+# --------------- THE ACTION ---------------
+# - shouldn't need to change this section
+
+# Compile turbsim binaries if needed
+for turbfile in wind_binaries:
+    if os.path.exists(turbfile):
+        print('Turbsim binary file {} exists, moving on...'.format(turbfile))
+    else:
+        # Run turbsim
+        turbinput = turbfile.split('.')[:-1][0] + '.inp'
+        fast_io.run_openfast(os.getcwd(), fastcall=turbsim_call, fastfile=turbinput)
+
 
 # Run test cases
-for case in testcases:
+for case in test_cases:
     # Run
     OpenFAST_dir = case
-    fast_io.run_openfast(OpenFAST_dir,fastcall=fastcall, chdir=True)
+    fast_io.run_openfast(OpenFAST_dir,fastcall=openfast_call, chdir=True)
 
 # Plot the results
-for cid, case in enumerate(testcases):
+for (cid, case), key in zip(enumerate(test_cases),plot_categories.keys()):
     # Load outdata
-    filename = os.path.join(case,'{}.outb'.format(case))
+    filename = os.path.join(case,'{}.out'.format(case))
     print(filename)
     allinfo, alldata = fast_io.load_output(filename)
 
     # Plot some results
-    fast_io.plot_fast_out(cases, allinfo, alldata,showplot=False,fignum=cid)
+    plot_case = {}
+    plot_case[key] = plot_categories[key]
+    fast_io.plot_fast_out(plot_case, allinfo,
+                          alldata, showplot=False)
 
 plt.show()
