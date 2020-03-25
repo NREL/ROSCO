47a065bcb8f9a2a7641a472a55ad74a2abef2f96
Nikhar Abbas <nikhar.abbas@colorado.edu>
2020-03-25 08:55:30 -0600

    Minimum max yaw limit

--- src/ControllerBlocks.f90
+++ src/ControllerBlocks.f90
@@ -320,7 +320,8 @@ CONTAINS
             ELSE 
                 MaxYaw = YawSD_Slope * LocalVar%WE_Vw + Offset ! In Degrees
             ENDIF
-
+            MaxYaw = max(MaxYaw, 50)
+            
             ! Shutdown?
             IF (LocalVar%Time > 30.0) THEN
                 IF (SD_BlPitchF > CntrPar%SD_MaxPit) THEN
