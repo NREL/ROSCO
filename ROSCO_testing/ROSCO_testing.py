bd39933973b1a52bb127286b83528c1670926cb9
Nikhar Abbas <nikhar.abbas@colorado.edu>
2020-07-14 09:02:06 -0600

    ROSCO testing as class, rename file

--- ROSCO_testing/ROSCO_testing.py
+++ ROSCO_testing/ROSCO_testing.py
@@ -138,7 +138,7 @@ class ROSCO_testing():
         iec.Turbsim_exe = self.Turbsim_exe
         iec.debug_level = self.debug_level
         iec.cores = self.cores
-        iec.run_dir = self.runDir
+        iec.run_dir = os.path.join(self.runDir,'wind')
         iec.overwrite = self.overwrite
         # iec.overwrite       = False
         if self.cores > 1:
