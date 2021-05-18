c4b1ccf9d78f084faea34d3d9fb9fc77432740fa
Nikhar Abbas <nikhar.abbas@colorado.edu>
2021-05-18 09:37:25 -0600

    Add helper functions

--- ROSCO_toolbox/linear/linear_models.py
+++ ROSCO_toolbox/linear/linear_models.py
@@ -13,6 +13,7 @@ import matplotlib.pyplot as plt
 import re
 from itertools import chain
 from scipy.io import loadmat
+import copy
 
 import os
 
@@ -727,44 +728,12 @@ def pc_openloop(linturb, controller, u):
 
     # Interpolate plant and controller
     P = interp_plant(linturb, u)
-    Cs = interp_controller(controller, u)
+    Cs = interp_pitch_controller(controller, u)
 
     # Combine controller and plant
     sys_ol = Cs*P
 
     return sys_ol
-
-
-def sensitivity(linturb, controller, u):
-    '''
-    This function finds the sensitivity function for the wind turbine with a pitch controller
-    
-    Inputs
-    ------
-    linturb: object
-        LinearTurbineModel object
-    controller: object
-        ROSCO toolbox controller object
-    u: float
-        wind speed to evaluate system at
-    '''
-
-    if linturb.B_ops.shape[1] > 1:
-        raise InputError(
-            'OpenLoop system calculations are only supported for single input systems, please run LinearTurbine.trim_system with len(desInputs) = 1')
-    if linturb.C_ops.shape[0] > 1:
-        raise InputError(
-            'OpenLoop system calculations are only supported for single output systems, please run LinearTurbine.trim_system with len(desOutputs) = 1')
-
-    # Interpolate plant and controller
-    P = interp_plant(linturb, u)
-    Cs = interp_controller(controller, u)
-
-    # Calculate sensitivity function
-    sens = feedback(1, Cs*P)
-
-    return sens
-
 
 def pc_closedloop(linturb, controller, u):
     '''
@@ -792,12 +761,43 @@ def pc_closedloop(linturb, controller, u):
 
     # Interpolate plant and controller
     P = interp_plant(linturb, u)
-    Cs = interp_controller(controller, u)
+    Cs = interp_pitch_controller(controller, u)
 
     # Combine controller and plant
     sys_cl = feedback(1, Cs*P)
 
     return sys_cl
+
+def pc_sensitivity(linturb, controller, u):
+    '''
+    This function finds the sensitivity function for the wind turbine with a pitch controller
+    
+    Inputs
+    ------
+    linturb: object
+        LinearTurbineModel object
+    controller: object
+        ROSCO toolbox controller object
+    u: float
+        wind speed to evaluate system at
+    '''
+
+    if linturb.B_ops.shape[1] > 1:
+        raise InputError(
+            'OpenLoop system calculations are only supported for single input systems, please run LinearTurbine.trim_system with len(desInputs) = 1')
+    if linturb.C_ops.shape[0] > 1:
+        raise InputError(
+            'OpenLoop system calculations are only supported for single output systems, please run LinearTurbine.trim_system with len(desOutputs) = 1')
+
+    # Interpolate plant and controller
+    P = interp_plant(linturb, u)
+    Cs = interp_pitch_controller(controller, u)
+
+    # Calculate sensitivity function
+    sens = feedback(1, Cs*P)
+
+    return sens
+
 
 
 def interp_plant(linturb, v, return_scipy=True):
@@ -833,7 +833,7 @@ def interp_plant(linturb, v, return_scipy=True):
     return P
 
 
-def interp_controller(controller, v, return_scipy=True):
+def interp_pitch_controller(controller, v, return_scipy=True):
     '''
     Interpolate ROSCO toolbox pitch controller and return linear model
 
@@ -864,13 +864,118 @@ def interp_controller(controller, v, return_scipy=True):
 
     return Cs
 
+def add_pcomp(linturb, k_float):
+    '''
+    Incorporate the effect of a parallel compensation term to a linear turbine model.
+
+    Inputs
+    ------
+    linturb: object
+        LinearTurbineModel object
+    k_float: float
+        parallel compensation feedback gain
+
+    Returns
+    -------
+    linturb2: object
+        Modified linturb with parallel compensation term included
+    '''
+
+    state_str = 'derivative of 1st tower fore-aft'
+    state_idx = np.flatnonzero(np.core.defchararray.find(
+        linturb.DescStates, state_str) > -1).tolist()
+    input_str = 'collective blade-pitch'
+    input_idx = np.flatnonzero(np.core.defchararray.find(
+        linturb.DescCntrlInpt, input_str) > -1).tolist()
+
+    K = np.zeros((linturb.B_ops.shape[1], linturb.A_ops.shape[1], linturb.A_ops.shape[2]))
+    K[input_idx, state_idx, :] = k_float
+
+    linturb2 = copy.copy(linturb)
+    linturb2.A_ops = linturb.A_ops + linturb.B_ops * K
+
+    return linturb2
+
 
 def interp_matrix(x, matrix_3D, q):
     q = np.clip(q, x.min(), x.max())
     f_m = sp.interpolate.interp1d(x, matrix_3D)
 
-    return f_m(q)
 
+    return f_m(np.squeeze(q))
+
+
+def _convert_to_ss(sys):
+    if isinstance(sys, (int, float, complex, np.float)):
+        ss = sp.signal.StateSpace([0], [0], [0], sys)
+    else:
+        ss = sys
+
+    return ss
+
+
+def feedback(sys1, sys2, sign=-1):
+    """
+    Feedback interconnection between two LTI systems.
+
+    This underlying methods here are pulled from the python 
+    control toolbox, but made to work with scipy state space 
+    objects.
+    """
+    sys1 = _convert_to_ss(sys1)
+    sys2 = _convert_to_ss(sys2)
+
+    # Check to make sure the dimensions are OK
+    if (sys1.inputs != sys2.outputs) or (sys1.outputs != sys2.inputs):
+        raise ValueError("State space systems don't have compatible "
+                         "inputs/outputs for feedback.")
+    if sys1.dt != sys2.dt:
+        raise ValueError(
+            "Both state space systems must have the same timestep (dt), or both be continuous time.")
+    else:
+        dt = sys1.dt
+
+    A1 = sys1.A
+    B1 = sys1.B
+    C1 = sys1.C
+    D1 = sys1.D
+    A2 = sys2.A
+    B2 = sys2.B
+    C2 = sys2.C
+    D2 = sys2.D
+
+    F = np.eye(sys1.inputs) - sign * np.dot(D2, D1)
+    if np.linalg.matrix_rank(F) != sys1.inputs:
+        raise ValueError("I - sign * D2 * D1 is singular to working precision.")
+
+    # Precompute F\D2 and F\C2 (E = inv(F))
+    # We can solve two linear systems in one pass, since the
+    # coefficients matrix F is the same. Thus, we perform the LU
+    # decomposition (cubic runtime complexity) of F only once!
+    # The remaining back substitutions are only quadratic in runtime.
+    E_D2_C2 = np.linalg.solve(F, np.concatenate((D2, C2), axis=1))
+    E_D2 = E_D2_C2[:, :sys2.inputs]
+    E_C2 = E_D2_C2[:, sys2.inputs:]
+
+    T1 = np.eye(sys1.outputs) + sign * np.dot(D1, E_D2)
+    T2 = np.eye(sys1.inputs) + sign * np.dot(E_D2, D1)
+
+    A = np.concatenate(
+        (np.concatenate(
+            (A1 + sign * np.dot(np.dot(B1, E_D2), C1),
+             sign * np.dot(B1, E_C2)), axis=1),
+         np.concatenate(
+             (np.dot(B2, np.dot(T1, C1)),
+              A2 + sign * np.dot(np.dot(B2, D1), E_C2)), axis=1)),
+        axis=0)
+    B = np.concatenate((np.dot(B1, T2), np.dot(np.dot(B2, D1), T2)), axis=0)
+    C = np.concatenate((np.dot(T1, C1), sign * np.dot(D1, E_C2)), axis=1)
+    D = np.dot(D1, T2)
+
+    if dt:
+        return sp.signal.StateSpace(A, B, C, D, dt=dt)
+    else:
+        return sp.signal.StateSpace(A, B, C, D)
 
 def deg2rad(d):
     return d * np.pi / 180
