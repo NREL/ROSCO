3ea48e29e9c28c45310f7b7238e547b38881f665
dzalkind <dzalkind@nrel.gov>
2021-06-25 13:59:28 -0600

    Fix and point example_01 to Tune_Cases/

--- Examples/example_01.py
+++ Examples/example_01.py
@@ -19,7 +19,8 @@ from ROSCO_toolbox import turbine as ROSCO_turbine
 
 # Load yaml file
 this_dir = os.path.dirname(os.path.abspath(__file__))
-parameter_filename = os.path.join(this_dir,'NREL5MW_example.yaml')
+tune_dir =  os.path.join(this_dir,'../Tune_Cases')
+parameter_filename = os.path.join(tune_dir,'NREL5MW.yaml')
 inps = yaml.safe_load(open(parameter_filename))
 path_params         = inps['path_params']
 turbine_params      = inps['turbine_params']
@@ -29,9 +30,9 @@ turbine = ROSCO_turbine.Turbine(turbine_params)
 
 turbine.load_from_fast(
     path_params['FAST_InputFile'],
-    os.path.join(this_dir,path_params['FAST_directory']),
+    os.path.join(tune_dir,path_params['FAST_directory']),
     dev_branch=True,
-    rot_source='txt',txt_filename=os.path.join(this_dir,path_params['rotor_performance_filename'])
+    rot_source='txt',txt_filename=os.path.join(tune_dir,path_params['rotor_performance_filename'])
     )
 
 # Print some basic turbine info
