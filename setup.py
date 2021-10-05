dbbe5a9a12c5798e3b0bb79e3623959b5ada2b46
Rafael M Mudafort <rafmudaf@gmail.com>
2021-10-05 09:14:12 -0500

    Remove unused input to setup function

--- setup.py
+++ setup.py
@@ -188,7 +188,6 @@ metadata = dict(
     url                           = URL,
     install_requires              = REQUIRED,
     python_requires               = REQUIRES_PYTHON,
-    # include_package_date          = True,
     packages                      = find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
     license                       = 'Apache License, Version 2.0',
     cmdclass                      = {'build_ext': CMakeBuildExt, 'upload': UploadCommand},
