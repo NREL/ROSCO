185f0cb04547675b3c23fecd3baaaa7f19f5457d
pibo <ptrbortolotti@gmail.com>
2021-10-29 16:12:45 -0600

    enlarge default range pitch and tsr

--- ROSCO_toolbox/turbine.py
+++ ROSCO_toolbox/turbine.py
@@ -254,8 +254,8 @@ class Turbine():
             self.load_blade_info()
         
         # Generate the look-up tables, mesh the grid and flatten the arrays for cc_rotor aerodynamic analysis
-        TSR_initial = np.arange(3, 15,0.5)
-        pitch_initial = np.arange(-1,25,0.5)
+        TSR_initial = np.arange(2, 15,0.5)
+        pitch_initial = np.arange(-5,25,0.5)
         pitch_initial_rad = pitch_initial * deg2rad
         ws_array = np.ones_like(TSR_initial) * self.v_rated # evaluate at rated wind speed
         omega_array = (TSR_initial * ws_array / self.rotor_radius) * RadSec2rpm
