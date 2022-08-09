6b9d482db6cada715be39ab2bf26cb9944d3b8fe
dzalkind <dzalkind@nrel.gov>
2022-08-09 09:57:10 -0600

    Fix version numbering

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ URL = 'https://github.com/NREL/ROSCO'
 EMAIL = 'nikhar.abbas@nrel.gov'
 AUTHOR = 'NREL, National Wind Technology Center'
 REQUIRES_PYTHON = '>=3.4'
-VERSION = '2.5.0'
+VERSION = '2.6.0'
 
 # These packages are required for all of the code to be executed. 
 # - Maybe you can get away with older versions...
