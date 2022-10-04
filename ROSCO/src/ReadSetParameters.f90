a6d4d90628ee495bac338674eacacb60c34921cd
dzalkind <dzalkind@nrel.gov>
2022-10-04 14:19:27 -0600

    Read NacHeading from OpenFAST

--- ROSCO/src/ReadSetParameters.f90
+++ ROSCO/src/ReadSetParameters.f90
@@ -50,6 +50,7 @@ CONTAINS
         LocalVar%rootMOOP(1)        = avrSWAP(30)
         LocalVar%rootMOOP(2)        = avrSWAP(31)
         LocalVar%rootMOOP(3)        = avrSWAP(32)
+        LocalVar%NacHeading         = avrSWAP(37) * R2D
         LocalVar%FA_Acc             = avrSWAP(53)
         LocalVar%NacIMU_FA_Acc      = avrSWAP(83)
         LocalVar%Azimuth            = avrSWAP(60)
