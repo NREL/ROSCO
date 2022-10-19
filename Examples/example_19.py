6ab46ee07b0d2d1dc2d8ee10bd47d2f0bc167f3f
dzalkind <dzalkind@nrel.gov>
2022-10-19 15:59:38 -0600

    Add SCADA to example_19, needs tuning

--- Examples/example_19.py
+++ Examples/example_19.py
@@ -13,8 +13,11 @@ from ROSCO_toolbox.ofTools.case_gen import CaseLibrary as cl
 from ROSCO_toolbox.ofTools.fast_io import output_processing
 from ROSCO_toolbox.controller import OpenLoopControl
 import numpy as np
+import pandas as pd
 
-
+OPENFAST = False
+RPS2RPM          = 9.5492966
+GB_RATIO = 111.7
 
 #directories
 this_dir            = os.path.dirname(os.path.abspath(__file__))
@@ -29,12 +32,97 @@ elif platform.system() == 'Darwin':
 else:
     lib_name = os.path.realpath(os.path.join(this_dir, '../ROSCO/build/libdiscon.so'))
 
+def read_scada(scada_file,mbc=False):
+    # try:
+    df = pd.read_csv(scada_file)
+
+
+    df.rename(columns = {
+        'Time (s)':'Time',
+        'Gen Power (kW)': 'GenPwr',
+        'Root Bending Flap 1 (kN-m)': 'RootMyb1',
+        'Root Bending Flap 2 (kN-m)': 'RootMyb2',
+        'Root Bending Flap 3 (kN-m)': 'RootMyb3',
+        'Rotor Speed (rpm)': 'RotSpd',
+        'Gen Torque (kN-m)':'GenTq',
+        'Azimuth (deg.)':'Azimuth',
+        'Pitch 1 (deg.)':'BldPitch1',
+        'Pitch 2 (deg.)':'BldPitch2',
+        'Pitch 3 (deg.)':'BldPitch3',
+        'Yaw Misalignment (deg.)': 'YawMisalign',
+    },inplace=True)
+
+    # Calculate generator torque 
+
+    GenSpd = df.RotSpd / RPS2RPM * GB_RATIO
+
+    GenTq = df.GenPwr/GenSpd
+
+    df['GenTq'] = GenTq
+
+    # remove pitch offset, assume BldPitch1 is true for now
+
+    offset_2 = np.mean(df.BldPitch1 - df.BldPitch2)
+    offset_3 = np.mean(df.BldPitch1 - df.BldPitch3)
+
+    df.BldPitch2 = df.BldPitch2 + offset_2
+    df.BldPitch3 = df.BldPitch3 + offset_3
+
+    if mbc:
+        # MBC Transform
+        n = 1
+        pi = np.pi
+
+        phi2 = 2/3 * pi
+        phi3 = 4/3 * pi
+
+        M_tilt = np.zeros(len(df))
+        M_yaw = np.zeros(len(df))
+
+        B_tilt = np.zeros(len(df))
+        B_yaw = np.zeros(len(df))
+
+        for i, d in enumerate(df.iterrows()):
+            d = d[1]
+
+            az = np.radians(d.Azimuth)
+
+            M = [d.RootMyb1,d.RootMyb2,d.RootMyb3]
+
+
+            B = np.array([d.BldPitch1,d.BldPitch2,d.BldPitch3])
+            B = B - np.mean(B)
+
+
+            M_tilt[i] = 2/3 * (np.cos(n *az) * M[0] + np.cos(n*(az - phi2)) * M[1] + np.cos(n*(az - phi3)) * M[2])
+            M_yaw[i] =  2/3 * (np.sin(n *az) * M[0] + np.sin(n*(az - phi2)) * M[1] + np.sin(n*(az - phi3)) * M[2]) 
+
+            B_tilt[i] = 2/3 * (np.cos(n *az) * B[0] + np.cos(n*(az - phi2)) * B[1] + np.cos(n*(az - phi3)) * B[2])
+            B_yaw[i] =  2/3 * (np.sin(n *az) * B[0] + np.sin(n*(az - phi2)) * B[1] + np.sin(n*(az - phi3)) * B[2]) 
+
+
+        df['RootMTilt'] = M_tilt
+        df['RootMYaw'] = M_yaw 
+
+        df['B_Tilt'] = B_tilt
+        df['B_Yaw'] = B_yaw 
+
+
+        # Compute summary data
+
+        # df_summ = df_summ.append({
+        #     'RootMyb1_std': df.RootMyb1.std(),
+        #     'IPC_act': np.sum(df['B_Tilt'] > 0.75) / len(df['B_Tilt']),
+        # },ignore_index=True)
+        
+        
+    return df
 
 def main():
 
     # Ensure external control paths are okay
-    parameter_filename = os.path.join(rosco_dir,'Tune_Cases/IEA15MW.yaml')
-    run_dir = os.path.join(example_out_dir,'19_RotPos_Turb_0')
+    parameter_filename = '/Users/dzalkind/Projects/RAAW/RAAW_OpenFAST/ROSCO/RAAW_rosco_BD.yaml'
+    run_dir = os.path.join(example_out_dir,'19_RotPos_SCADA_0')
     os.makedirs(run_dir,exist_ok=True)
 
     # Case input for RotSpeed IC
@@ -61,16 +149,38 @@ def main():
 
     # Gather azimuth, blade p
     # itch, generator torque output
-    op = output_processing.output_processing()
-    fast_out = op.load_fast_out(os.path.join(example_out_dir,'19_RotPos_Turb_0/IEA15MW/turb_bts/base/IEA15MW_0.outb'), tmin=0)
-    
-    olc = OpenLoopControl()
-    olc.ol_timeseries['time'] = fast_out[0]['Time']
-    olc.ol_timeseries['blade_pitch1'] = np.radians(fast_out[0]['BldPitch1'])
-    olc.ol_timeseries['blade_pitch2'] = np.radians(fast_out[0]['BldPitch2'])
-    olc.ol_timeseries['blade_pitch3'] = np.radians(fast_out[0]['BldPitch3'])
-    olc.ol_timeseries['generator_torque'] = fast_out[0]['GenTq'] * 1000
-    olc.ol_timeseries['azimuth'] = np.radians(fast_out[0]['Azimuth'])
+    if OPENFAST:
+        op = output_processing.output_processing()
+        fast_out = op.load_fast_out(os.path.join(example_out_dir,'19_RotPos_Turb_0/IEA15MW/turb_bts/base/IEA15MW_0.outb'), tmin=0)
+        
+        olc = OpenLoopControl()
+        olc.ol_timeseries['time'] = fast_out[0]['Time']
+        olc.ol_timeseries['blade_pitch1'] = np.radians(fast_out[0]['BldPitch1'])
+        olc.ol_timeseries['blade_pitch2'] = np.radians(fast_out[0]['BldPitch2'])
+        olc.ol_timeseries['blade_pitch3'] = np.radians(fast_out[0]['BldPitch3'])
+        olc.ol_timeseries['generator_torque'] = fast_out[0]['GenTq'] * 1000
+        olc.ol_timeseries['azimuth'] = np.radians(fast_out[0]['Azimuth'])
+    else:
+        # print('here')
+        scada_file = '/Users/dzalkind/Box/EXT - RAAW data sharing/GE Proprietary/Data Assimilation (Mid-Fidelity)/Historical Data/v31/_Diagnostics/_GeneralDiagnostic/_TurbineConditions_Timeseries_Bin762.csv'
+        df = read_scada(scada_file)
+                
+        # Simple integrate
+        Az_unwrap = np.unwrap(np.radians(df.Azimuth))
+        tt = df.Time
+        omega = df.RotSpd / 9.5492966
+
+        int_RotSpeed = np.empty(len(omega))
+        for i_t in range(len(omega)):
+            int_RotSpeed[i_t] = np.trapz(omega[:i_t],tt[:i_t]) + Az_unwrap[0]
+
+        olc = OpenLoopControl()
+        olc.ol_timeseries['time'] = tt.to_numpy()
+        olc.ol_timeseries['blade_pitch1'] = np.radians(df.BldPitch1).to_numpy()
+        olc.ol_timeseries['blade_pitch2'] = np.radians(df.BldPitch2).to_numpy()
+        olc.ol_timeseries['blade_pitch3'] = np.radians(df.BldPitch3).to_numpy()
+        olc.ol_timeseries['generator_torque'] = df.GenTq * 1000
+        olc.ol_timeseries['azimuth'] = int_RotSpeed
 
     # set up control_params for next run
     open_loop = olc.write_input(os.path.join(run_dir,'ol_input.dat'))
@@ -79,7 +189,8 @@ def main():
     controller_params['OL_Mode'] = 2
     controller_params['PA_Mode'] = 0
     controller_params['DISCON'] = {}
-    controller_params['DISCON']['RP_Gains'] = [-3e7,-3e6,-3e8]
+    controller_params['DISCON']['RP_Gains'] = -3e4 * np.array([1,1e-1,1e1])
+    controller_params['DISCON']['PC_MinPit'] = np.radians(-20)
 
 
     # run again with slower IC and rotor position control
@@ -87,10 +198,15 @@ def main():
     r.case_inputs   = case_inputs
     r.base_name     = 'rpc'
     r.wind_case_opts    = {
-        'TMax': 300,
-        'wind_filenames': ['/Users/dzalkind/Tools/WEIS-1/outputs/02_RAAW_TurbOff/wind/IEA15_NTM_U8.500000_Seed123.0.bts']
+        'TMax': 150,
+        'wind_filenames': ['/Users/dzalkind/Projects/RAAW/RAAW_OpenFAST/wind/bin762-turbsim.bts']
         }
+    # Set initial conditions
+    r.case_inputs = {}
+    r.case_inputs[("ElastoDyn","RotSpeed")]      = {'vals':[df.RotSpd[0]], 'group':0}
+    r.case_inputs[("ElastoDyn","Azimuth")]      = {'vals':[np.degrees(int_RotSpeed[0])], 'group':0}
     r.controller_params = controller_params
+    r.openfast_exe = '/Users/dzalkind/opt/anaconda3/envs/rosco-env2/bin/openfast'
     r.run_FAST()
 
 
