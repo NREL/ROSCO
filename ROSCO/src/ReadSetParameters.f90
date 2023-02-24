baaa8db069199cb25116a73365ff6d63be5fa065
kbrown1snl <66843135+kbrown1snl@users.noreply.github.com>
2023-02-24 11:40:52 -0700

    Update ReadSetParameters.f90
    Update units.

--- ROSCO/src/ReadSetParameters.f90
+++ ROSCO/src/ReadSetParameters.f90
@@ -421,7 +421,7 @@ CONTAINS
         CALL ReadEmptyLine(UnControllerParameters,CurLine)  
         CALL ParseInput(UnControllerParameters,CurLine,'AWC_NumModes',  accINFILE(1),CntrPar%AWC_NumModes, ErrVar)
         CALL ParseAry(  UnControllerParameters,CurLine,'AWC_n',         CntrPar%AWC_n,     CntrPar%AWC_NumModes, accINFILE(1), ErrVar)
-        CALL ParseAry(  UnControllerParameters,CurLine,'AWC_omega',     CntrPar%AWC_omega, CntrPar%AWC_NumModes, accINFILE(1), ErrVar)
+        CALL ParseAry(  UnControllerParameters,CurLine,'AWC_freq',     CntrPar%AWC_freq, CntrPar%AWC_NumModes, accINFILE(1), ErrVar)
         CALL ParseAry(  UnControllerParameters,CurLine,'AWC_amp',       CntrPar%AWC_amp,   CntrPar%AWC_NumModes, accINFILE(1), ErrVar)
         CALL ParseAry(  UnControllerParameters,CurLine,'AWC_clockangle',CntrPar%AWC_clockangle,CntrPar%AWC_NumModes, accINFILE(1), ErrVar)
         CALL ReadEmptyLine(UnControllerParameters,CurLine)
