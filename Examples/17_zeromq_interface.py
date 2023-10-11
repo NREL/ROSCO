b2cd17973379bad9cb77be847b5277c72eded222
dzalkind <dzalkind@nrel.gov>
2023-10-11 10:36:08 -0600

    Make tuning yaml, zmq example consistent with others

--- Examples/17_zeromq_interface.py
+++ Examples/17_zeromq_interface.py
@@ -96,7 +96,7 @@ def sim_rosco():
 
     # Load turbine data from OpenFAST and rotor performance text file
     cp_filename = os.path.join(
-        tune_dir, path_params['FAST_directory'], path_params['rotor_performance_filename'])
+        tune_dir, path_params['rotor_performance_filename'])
     turbine.load_from_fast(
         path_params['FAST_InputFile'],
         os.path.join(tune_dir, path_params['FAST_directory']),
