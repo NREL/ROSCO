1ebd357a381b015bd629d83c24b4bb685798dff5
Garrett Barter <garrett.barter@nrel.gov>
2024-01-05 14:54:13 -0700

    elevate pyzmq to be installed before extension is built, but also remove option and make it something that works if it is found

--- setup.py
+++ setup.py
@@ -68,7 +68,6 @@ class CMakeBuildExt(build_ext):
             cmake_args = ['-DBUILD_SHARED_LIBS=OFF']
             cmake_args += ['-DCMAKE_Fortran_FLAGS=-ffree-line-length-0']
             cmake_args += ['-DCMAKE_INSTALL_PREFIX={}'.format(this_directory)]
-            cmake_args += ['-DZMQ_CLIENT=ON']
             if "CONDA_PREFIX" in os.environ:
                 cmake_args += [f"-DCMAKE_PREFIX_PATH={os.environ['CONDA_PREFIX']}"]
                 
