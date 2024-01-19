0f3116c230f59a316b65697c48fb9dc583cb526d
Garrett Barter <garrett.barter@nrel.gov>
2024-01-19 14:58:24 -0700

    improvements for conda file structure on windows

--- rosco/__init__.py
+++ rosco/__init__.py
@@ -13,8 +13,10 @@ rosco_dir = os.path.dirname( os.path.abspath(__file__) )
 libname = "libdiscon"
 
 lib_path = [os.path.join(rosco_dir, "lib", libname+lib_ext), # pip installs (regular and editable)
-            os.path.join(rosco_dir, "..", "..", "local", "lib", libname+lib_ext), # WEIS library
+            os.path.join(os.path.dirname( os.path.dirname( rosco_dir )), "local", "lib", libname+lib_ext), # WEIS library
             os.path.join(os.path.dirname( sysconfig.get_path('stdlib') ), libname+lib_ext), # conda installs
+            os.path.join(os.path.dirname( sysconfig.get_path('stdlib') ), "lib", libname+lib_ext), # conda installs
+            os.path.join(os.path.dirname( sysconfig.get_path('stdlib') ), "Library", "lib", libname+lib_ext), # conda installs
             os.path.join( sysconfig.get_path('platlib'), "rosco", "lib", libname+lib_ext), # system-wide pip installs
             os.path.join( sysconfig.get_config_var("userbase"), "lib", "python", "site-packages", "rosco", "lib", libname+lib_ext), # system wide local
             ]
