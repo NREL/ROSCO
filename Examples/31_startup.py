1e256fc15d2c0d9108808e108e4be9a4b1e94255
AbhineetGupta <abhineet.gupta@nrel.gov>
2025-02-28 11:14:10 -0700

    Startup: Modify startup example-31 to include resonable values of startup parameters

--- Examples/31_startup.py
+++ Examples/31_startup.py
@@ -15,7 +15,7 @@ from rosco.toolbox.ofTools.case_gen import CaseLibrary as cl
 from rosco.toolbox.ofTools.fast_io import output_processing
 
 rpm2RadSec = 2.0 * (np.pi) / 60.0
-deg2rad = np.pi/180.0
+deg2rad = np.pi / 180.0
 
 # directories
 this_dir = os.path.dirname(os.path.abspath(__file__))
@@ -33,15 +33,14 @@ def main():
     controller_params["DISCON"]["Echo"] = 1
     controller_params["LoggingLevel"] = 3
     controller_params["SU_Mode"] = 1
-    controller_params["DISCON"]["SU_FW_Pitch"] = 15.0 * np.pi/180.0
+    controller_params["DISCON"]["SU_FW_Pitch"] = 20.0 * np.pi / 180.0
     controller_params["DISCON"]["SU_FW_MinDuration"] = 70.0
-    controller_params["DISCON"]["SU_RotorSpeedThresh"] = 4*(2*np.pi)/60.0
-    
+    controller_params["DISCON"]["SU_RotorSpeedThresh"] = 4 * (2 * np.pi) / 60.0
+
     controller_params["DISCON"]["SU_LoadStages_N"] = 2
     controller_params["DISCON"]["SU_LoadStages"] = [0.4, 1]
-    controller_params["DISCON"]["SU_LoadRampDuration"] = [20,20]
-    controller_params["DISCON"]["SU_LoadHoldDuration"] = [40,20]
-
+    controller_params["DISCON"]["SU_LoadRampDuration"] = [30, 20]
+    controller_params["DISCON"]["SU_LoadHoldDuration"] = [30, 20]
 
     # simulation set up
     r = run_FAST_ROSCO()
@@ -56,8 +55,7 @@ def main():
     r.case_inputs[("ElastoDyn", "PtfmPDOF")] = {"vals": ["False"], "group": 0}
     r.case_inputs[("ElastoDyn", "PtfmYDOF")] = {"vals": ["False"], "group": 0}
 
-
-    t_max = 300
+    t_max = 200
 
     run_dir = os.path.join(example_out_dir, "31_startup_demo/1_pitch")
 
@@ -74,7 +72,7 @@ def main():
     r.case_inputs[("ElastoDyn", "BlPitch3")] = {"vals": [90], "group": 0}
     r.case_inputs[("ElastoDyn", "RotSpeed")] = {"vals": [0.0], "group": 0}
 
-    # Run simulation 
+    # Run simulation
     os.makedirs(run_dir, exist_ok=True)
     r.controller_params = controller_params
     r.save_dir = run_dir
