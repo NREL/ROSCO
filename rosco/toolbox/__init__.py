2dd4820152bb3426ff6f494dfbcfde66337093c0
Garrett Barter <garrett.barter@nrel.gov>
2025-09-11 10:56:08 -0600

    py313 robustness improvements

--- rosco/toolbox/__init__.py
+++ rosco/toolbox/__init__.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 """Top-level package for ROSCO Repo."""
-import pkg_resources
+import importlib
+import importlib.metadata
 
 __author__ = """Nikhar J. Abbas and Daniel S. Zalkind"""
 __email__ = 'daniel.zalkind@nrel.gov'
-__version__ = pkg_resources.get_distribution('rosco').version
+__version__ = importlib.metadata.version('rosco')
+
